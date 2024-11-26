1797020a7ab86b6cfa8d13ca933bc18ea1432db9
RuthNyankum <rnyankum99@gmail.com>
2024-11-26 16:25:46 +0000

    project initial

---
+++ src/pages/home/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      Home
+      <h1 className="font-bold text-7xl text-white bg-black">FARMER FRIEND</h1>
+    </div>
+  );
+};
+
+export default Home;
